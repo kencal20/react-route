0a9426ea928bfa208f7c68b3876570a411d5707a
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-08-05 08:47:52 +0000

    about js

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+             <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio esse
+        expedita adipisci, quam blanditiis harum ducimus quis tenetur quaerat?
+        Sint, laudantium accusamus reiciendis aut enim nobis asperiores suscipit
+        saepe numquam.
+        </p>
+        </div>
+    );
+}
+
+export default About;
