64b2deabe4fdfaa6d53a312e4db260c6efa4e5ba
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-08-05 08:48:27 +0000

    Home js

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio esse
+        expedita adipisci, quam blanditiis harum ducimus quis tenetur quaerat?
+        Sint, laudantium accusamus reiciendis aut enim nobis asperiores suscipit
+        saepe numquam.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
