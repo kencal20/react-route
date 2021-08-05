0f5b9aa5e93792f8f7d35c3eb66ceb60da9eb6f0
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-08-05 08:48:13 +0000

    contacts

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Contacts = () => {
+  return (
+    <div>
+      <h1>Contacts</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio esse
+        expedita adipisci, quam blanditiis harum ducimus quis tenetur quaerat?
+        Sint, laudantium accusamus reiciendis aut enim nobis asperiores suscipit
+        saepe numquam.
+      </p>
+    </div>
+  );
+};
+
+export default Contacts;
